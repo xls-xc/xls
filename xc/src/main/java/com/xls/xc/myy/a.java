e869149ef0967c8b0d1cc90cfd7522844f43d78b
chenshuaikai <17703751364@163.com>
2020-04-03 20:15:58 +0800

    测试

---
+++ xc/src/main/java/com/xls/xc/myy/a.java
@@ -0,0 +1,13 @@
+package com.xls.xc.myy;
+
+/**
+ * @ProjectName: xls (星辰)
+ * @PackageName: com.xls.xc.myy
+ * @ClassName: a
+ * @Description: 还没有添加类的描述信息
+ * @Author: SkyChen
+ * @Create: 2020-04-03 20:15
+ * @Version: v1.0
+ **/
+public class a {
+}
